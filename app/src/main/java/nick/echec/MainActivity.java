1b4036a5392cb8f0a24b24342564918c8db4f4ce
Nicholas <nickmarcil@hotmail.com>
2016-04-29 20:37:05 -0400

    reconnait piece + tour

--- app/src/main/java/nick/echec/MainActivity.java
+++ app/src/main/java/nick/echec/MainActivity.java
@@ -1,5 +1,6 @@
 package nick.echec;
 
+import android.content.res.Resources;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.graphics.Canvas;
@@ -12,6 +13,7 @@ import android.graphics.RectF;
 import android.graphics.drawable.ClipDrawable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.TypedValue;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
@@ -23,25 +25,35 @@ import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
     int cliqueX, cliqueY;
-   /* Button btn00, btn01, btn02, btn03, btn04, btn05, btn06, btn07,
-    btn10, btn11, btn12, btn13, btn14, btn15, btn16, btn17,
-    btn20, btn21, btn22, btn23, btn24, btn25, btn26, btn27,
-    btn30, btn31, btn32, btn33, btn34, btn35, btn36, btn37,
-    btn40, btn41, btn42, btn43, btn44, btn45, btn46, btn47,
-    btn50, btn51, btn52, btn53, btn54, btn55, btn56, btn57,
-    btn60, btn61, btn62, btn63, btn64, btn65, btn66, btn67,
-    btn70, btn71, btn72, btn73, btn74, btn75, btn76, btn77;*/
+    /* Button btn00, btn01, btn02, btn03, btn04, btn05, btn06, btn07,
+     btn10, btn11, btn12, btn13, btn14, btn15, btn16, btn17,
+     btn20, btn21, btn22, btn23, btn24, btn25, btn26, btn27,
+     btn30, btn31, btn32, btn33, btn34, btn35, btn36, btn37,
+     btn40, btn41, btn42, btn43, btn44, btn45, btn46, btn47,
+     btn50, btn51, btn52, btn53, btn54, btn55, btn56, btn57,
+     btn60, btn61, btn62, btn63, btn64, btn65, btn66, btn67,
+     btn70, btn71, btn72, btn73, btn74, btn75, btn76, btn77;*/
     RelativeLayout layout;
     ArrayList<MoveListener> lstMoveListener;
-    ImageView imgPN1, imgPN2, imgPN3, imgPN4, imgPN5, imgPN6, imgPN7, imgPN8;
-    ImageView imgPB1, imgPB2, imgPB3, imgPB4, imgPB5, imgPB6, imgPB7, imgPB8;
+    ImageView PN1, PN2, PN3, PN4, PN5, PN6, PN7, PN8;
+    ImageView PB1, PB2, PB3, PB4, PB5, PB6, PB7, PB8;
+    ImageView TN1, TN2, FN1, FN2, CN1, CN2, KN1, QN1;
+    ImageView TB1, TB2, FB1, FB2, CB1, CB2, KB1, QB1;
     ArrayList<ImageView> pionsNoirs, pionsBlancs;
     RelativeLayout.LayoutParams params;
+    Bitmap bitmap;
+    Resources r;
+    boolean[] premierTourPionsNoirs = new boolean[8];
+    boolean premierTourPionsBlancs[] = new boolean[8];
+    boolean tourBlanc = true;
+    ArrayList<String> mouvDispos;
+    String pieces[] = {"TN100", "CN101", "FN102", "KN103", "QN104", "FN205", "CN206", "TN207", "PN110", "PN211", "PN312", "PN413", "PN514", "PN615", "PN716","PN817",
+            "PB160", "PB261", "PB362", "PB463", "PB564", "PB665", "PB766","PB867", "TB170", "CB171", "FB172", "KB173", "QB174", "FB275", "CB276", "TB277"};
 
     public static Bitmap couperImage(Bitmap bitmap, int posx, int posy) {
         int topLx, topLy;
         topLx = posx * (bitmap.getWidth() / 6);
-        topLy =  posy * (bitmap.getHeight() / 2);
+        topLy = posy * (bitmap.getHeight() / 2);
         return Bitmap.createBitmap(bitmap, topLx, topLy, bitmap.getWidth() / 6, bitmap.getHeight() / 2);
     }
 
@@ -52,54 +64,203 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         lstMoveListener = new ArrayList<>();
         setContentView(R.layout.activity_main);
         layout = (RelativeLayout) findViewById(R.id.relative);
-        for (int j = 0; j < layout.getChildCount(); j++)
+        r = getResources();
+        mouvDispos = new ArrayList<>();
+        for(int i = 0;i < 8;i++)
         {
+            premierTourPionsBlancs[i] = true;
+            premierTourPionsNoirs[i] = true;
+        }
+        for (int j = 0; j < layout.getChildCount(); j++) {
             View v = layout.getChildAt(j);
-            if (v instanceof  Button)
-            {
+            if (v instanceof Button) {
                 v.setBackgroundColor(Color.TRANSPARENT);
                 v.setOnClickListener(this);
             }
         }
-        //top left == 5 left, 240 top; 2rangée ==
         pionsNoirs = new ArrayList<>();
-        imgPN1 =new ImageView(this);imgPN2 =new ImageView(this);imgPN3 =new ImageView(this);imgPN4 =new ImageView(this);imgPN5 =new ImageView(this);imgPN6 =new ImageView(this);imgPN7 =new ImageView(this);imgPN8 =new ImageView(this);
-        pionsNoirs.add(imgPN1);pionsNoirs.add(imgPN2);pionsNoirs.add(imgPN3);pionsNoirs.add(imgPN4);
-        pionsNoirs.add(imgPN5);pionsNoirs.add(imgPN6);pionsNoirs.add(imgPN7);pionsNoirs.add(imgPN8);
+        PN1 = new ImageView(this);
+        PN2 = new ImageView(this);
+        PN3 = new ImageView(this);
+        PN4 = new ImageView(this);
+        PN5 = new ImageView(this);
+        PN6 = new ImageView(this);
+        PN7 = new ImageView(this);
+        PN8 = new ImageView(this);
+        pionsNoirs.add(PN1);
+        pionsNoirs.add(PN2);
+        pionsNoirs.add(PN3);
+        pionsNoirs.add(PN4);
+        pionsNoirs.add(PN5);
+        pionsNoirs.add(PN6);
+        pionsNoirs.add(PN7);
+        pionsNoirs.add(PN8);
 
-        Bitmap bitmap = BitmapFactory.decodeResource(getResources(), R.drawable.pawns);
-        Bitmap bitmapTemp = couperImage(bitmap, 5, 0);
-        for(int i = 0; i < 8;i++)
-        {
-            params = new RelativeLayout.LayoutParams(180, 180);
-            pionsNoirs.get(i).setImageBitmap(bitmapTemp);
-            params.leftMargin = 5 + (112 * i);
-            params.topMargin = 240 + (112);
-            layout.addView(pionsNoirs.get(i), params);
+        bitmap = BitmapFactory.decodeResource(getResources(), R.drawable.pawns);
+        for (int i = 0; i < 8; i++) {
+            setPawn(pionsNoirs.get(i),i,1,5,0);
         }
-
-        bitmapTemp = couperImage(bitmap, 5, 1);
         pionsBlancs = new ArrayList<>();
-        imgPB1 =new ImageView(this);imgPB2 =new ImageView(this);imgPB3 =new ImageView(this);imgPB4 =new ImageView(this);imgPB5 =new ImageView(this);imgPB6 =new ImageView(this);imgPB7 =new ImageView(this);imgPB8 =new ImageView(this);
-        pionsBlancs.add(imgPB1);pionsBlancs.add(imgPB2);pionsBlancs.add(imgPB3);pionsBlancs.add(imgPB4);
-        pionsBlancs.add(imgPB5);pionsBlancs.add(imgPB6);pionsBlancs.add(imgPB7);pionsBlancs.add(imgPB8);
-        for(int i = 0; i < 8;i++)
-        {
-            params = new RelativeLayout.LayoutParams(180, 180);
-            pionsBlancs.get(i).setImageBitmap(bitmapTemp);
-            params.leftMargin = 5 + (112 * i);
-            params.topMargin = 240 + (112 * 6);
-            layout.addView(pionsBlancs.get(i), params);
+        PB1 = new ImageView(this);
+        PB2 = new ImageView(this);
+        PB3 = new ImageView(this);
+        PB4 = new ImageView(this);
+        PB5 = new ImageView(this);
+        PB6 = new ImageView(this);
+        PB7 = new ImageView(this);
+        PB8 = new ImageView(this);
+        pionsBlancs.add(PB1);
+        pionsBlancs.add(PB2);
+        pionsBlancs.add(PB3);
+        pionsBlancs.add(PB4);
+        pionsBlancs.add(PB5);
+        pionsBlancs.add(PB6);
+        pionsBlancs.add(PB7);
+        pionsBlancs.add(PB8);
+        for (int i = 0; i < 8; i++) {
+            setPawn(pionsBlancs.get(i),i,6,5,1);
         }
+        TN1 = new ImageView(this);
+        setPawn(TN1, 0, 0, 2, 0);
+        TN2 = new ImageView(this);
+        setPawn(TN2, 7, 0, 2, 0);
+        FN1 = new ImageView(this);
+        setPawn(FN1, 2, 0, 3, 0);
+        FN2 = new ImageView(this);
+        setPawn(FN2,5,0,3,0);
+        CN1 = new ImageView(this);
+        setPawn(CN1, 1, 0, 4, 0);
+        CN2 = new ImageView(this);
+        setPawn(CN2, 6, 0, 4, 0);
+        KN1 = new ImageView(this);
+        setPawn(KN1,3,0,1,0);
+        QN1 = new ImageView(this);
+        setPawn(QN1,4,0,0,0);
+        
+        TB1 = new ImageView(this);
+        setPawn(TB1,0,7,2,1);
+        TB2 = new ImageView(this);
+        setPawn(TB2,7,7,2,1);
+        FB1 = new ImageView(this);
+        setPawn(FB1,2,7,3,1);
+        FB2 = new ImageView(this);
+        setPawn(FB2,5,7,3,1);
+        CB1 = new ImageView(this);
+        setPawn(CB1,1,7,4,1);
+        CB2 = new ImageView(this);
+        setPawn(CB2,6,7,4,1);
+        KB1 = new ImageView(this);
+        setPawn(KB1,3,7,1,1);
+        QB1 = new ImageView(this);
+        setPawn(QB1,4,7,0,1);
 
 
+    }
+
+    public void setPawn(final ImageView img,int posX, int posY, int imgX, int imgY)
+    {
+        Bitmap bitmapTemp = couperImage(bitmap, imgX, imgY);
+        int px = (int) TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, 8 + (37 * posX), r.getDisplayMetrics());
+        int py = (int) TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, 108 + (37 * posY), r.getDisplayMetrics());
+        int test180 = (int) TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, 50, r.getDisplayMetrics());
+        params = new RelativeLayout.LayoutParams(test180, test180);
+        img.setImageBitmap(bitmapTemp);
+        params.leftMargin = px;
+        params.topMargin = py;
+        layout.addView(img, params);
     }
 
     @Override
     public void onClick(View v) {
         cliqueY = Character.getNumericValue(v.getResources().getResourceEntryName(v.getId()).charAt(3));
         cliqueX = Character.getNumericValue(v.getResources().getResourceEntryName(v.getId()).charAt(4));
-        Toast.makeText(getApplicationContext(),"btn" + cliqueY + cliqueX +" appuyer",Toast.LENGTH_SHORT).show();
+        for(String s : pieces)
+        {
+            String caseClique = String.valueOf(cliqueY) + String.valueOf(cliqueX);
+            String temp = String.valueOf(s.charAt(3)) + s.charAt(4);
+            if (caseClique.equals(temp))
+            {
+                String nomCouleur;
+                switch (s.charAt(1))
+                {
+                    case 'N':
+                        nomCouleur = "noir";
+                        break;
+                    case 'B':
+                        nomCouleur = "blanc";
+                        break;
+                    default:
+                        nomCouleur = "AUCUNECOULEUR";
+                        break;
+                }
+                boolean valide = (tourBlanc && nomCouleur == "blanc") ||(!tourBlanc && nomCouleur == "noir");
+                if(valide)
+                {
+                    String nomPiece;
+                    switch (s.charAt(0))
+                    {
+                        case 'T':
+                            nomPiece = "Tour";
+                            break;
+                        case 'C':
+                            nomPiece = "Cheval";
+                            break;
+                        case 'F':
+                            nomPiece = "Fou";
+                            break;
+                        case 'Q':
+                            nomPiece = "Reine";
+                            break;
+                        case 'K':
+                            nomPiece = "Roi";
+                            break;
+                        case 'P':
+                            nomPiece = "Pion";
+                            MouvPionPossible(cliqueX,cliqueY,s);
+                            break;
+                        default:
+                            nomPiece = "AUCUNNOM";
+                            break;
+
+                    }
+                    if(tourBlanc)
+                        layout.setBackgroundColor(Color.BLACK);
+                    else
+                        layout.setBackgroundColor(Color.WHITE);
+                    tourBlanc= !tourBlanc;
+                    Toast.makeText(getApplicationContext(),"mouv dispo: " + (mouvDispos.size() > 0? mouvDispos.get(0):null) + " " + (mouvDispos.size() > 1? mouvDispos.get(1):null),Toast.LENGTH_SHORT).show();
+                    mouvDispos.clear();
+                    return;
+                }
+
+            }
+        }
+    }
+
+    public void MouvPionPossible(int posx, int posy, String nom)
+    {
+        if(nom.charAt(1) == 'B')
+        {
+            int pos = (Character.getNumericValue(nom.charAt(2))) - 1;
+            if (premierTourPionsBlancs[pos])
+            {
+                for(int i = 0; i < 2;i++)
+                {
+                    if (posy - i >= 0)
+                    {
+                        for(int j = 0; j < pieces.length;j++)
+                        {
+                            if(Character.getNumericValue(pieces[j].charAt(3)) == posx && Character.getNumericValue(pieces[j].charAt(4)) == posy)
+                            {
+                                continue;
+                            }
+                        }
+                        mouvDispos.add(Integer.toString(posy - (i + 1)) + Integer.toString(posx));
+                    }
+
+                }
+            }
+        }
     }
 
     public void addMoveListener(MoveListener ml)
