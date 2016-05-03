0bc91783fe3e770d1d2c19900e88914673a991b2
nicholas <nickmarcil@hotmail.com>
2016-05-02 21:43:31 -0400

    regler 2 bug

--- app/src/main/java/nick/echec/MainActivity.java
+++ app/src/main/java/nick/echec/MainActivity.java
@@ -361,7 +361,8 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                     String posTemp = Character.toString(s2.charAt(3)) + Character.toString(s2.charAt(4));
                     if (mouvDispo.equals(posTemp))
                     {
-                        mouvCauseMort.add(s);
+                        if(pionEnMouvement.charAt(1) != s2.charAt(1))
+                            mouvCauseMort.add(s);
                     }
                 }
                 toRemove.add(s);
@@ -550,7 +551,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
             premierTourPionsBlancs[i] = true;
             premierTourPionsNoirs[i] = true;
         }
-        
+
         lstMoveListener = new ArrayList<>();
         setContentView(R.layout.activity_main);
         layout = (RelativeLayout) findViewById(R.id.relative);
@@ -624,8 +625,15 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                 v.setOnClickListener(this);
             }
         }
+        if(tourBlanc)
+            layout.setBackgroundColor(Color.BLACK);
+        else
+            layout.setBackgroundColor(Color.WHITE);
+        tourBlanc= !tourBlanc;
         pionsNoirs = new ArrayList<>();
-
+        String piecesTemp[] = {"TN100", "CN101", "FN102", "KN103", "QN104", "FN205", "CN206", "TN207", "PN110", "PN211", "PN312", "PN413", "PN514", "PN615", "PN716","PN817",
+                "PB160", "PB261", "PB362", "PB463", "PB564", "PB665", "PB766","PB867", "TB170", "CB171", "FB172", "KB173", "QB174", "FB275", "CB276", "TB277"};
+        pieces = piecesTemp;
         //Les images des pions et leur tag pour les reconnaître dans d'autre fonction
         PN1 = new ImageView(this);
         PN1.setTag("PN1");
