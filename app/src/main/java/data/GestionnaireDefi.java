41b5c250bfd796753356ca7ac100fd92b958638b
maxime <maxime_m@live.ca>
2016-05-04 11:38:17 -0400

    Gestionnaire de defi

---
+++ app/src/main/java/data/GestionnaireDefi.java
@@ -0,0 +1,17 @@
+package data;
+
+/**
+ * Gère les défis offerts aux utilisateurs
+ * Created by Maxime on 5/4/2016.
+ */
+public class GestionnaireDefi {
+    public static boolean ajouter(String nom, int nbTours, String grille) {
+        //TODO
+        throw new UnsupportedOperationException();
+    }
+
+    public static Defi get(String nom) {
+        //TODO
+        throw new UnsupportedOperationException();
+    }
+}
