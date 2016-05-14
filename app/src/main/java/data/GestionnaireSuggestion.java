35faebc0f3f2a285f56d563202ddf41cffb9618e
maxime <maxime_m@live.ca>
2016-05-14 17:37:53 -0400

    Ajout des suggestions dans la bd

--- app/src/main/java/data/GestionnaireSuggestion.java
+++ app/src/main/java/data/GestionnaireSuggestion.java
@@ -1,21 +1,46 @@
 package data;
 
+import android.content.ContentValues;
+import android.database.sqlite.SQLiteDatabase;
+
+import com.annimon.stream.Stream;
+
+import java.util.List;
+
 /**
  * Gère l'ajout des suggestions a la bd
  * Created by Maxime on 5/10/2016.
  */
 public class GestionnaireSuggestion {
     /**
-     * Ajoute une suggestion a la base de donnée
+     * Ajoute une ou plusieurs suggestion a la base de donnée
      *
      * @param u    Utilisateur qu'on ajoute a la bd
      * @param type type de défi a ajouter
      * @param diff niveau de difficulté
      * @return si la suggestion a été ajoutée
      */
-    public static boolean ajouter(Utilisateur u, Type_Defi type, float diff) {
-        //TODO
-        throw new UnsupportedOperationException("lol fuck you");
+    public static boolean ajouter(Utilisateur u, Type_Defi type, int diff) {
+        List<Defi> defis = GestionnaireDefi.get(diff);
+
+        return Stream.of(defis)
+                .filter(d -> d.getNom().toLowerCase().contains(type.name().toLowerCase()))
+                .map(d -> insertSuggestion(u.getId(), d.getId()))
+                .anyMatch(b -> b);
+
+    }
+
+    private static boolean insertSuggestion(int user, int defi) {
+        SQLiteDatabase db = MoteurBD.getMoteurBD().getDb();
+
+        if (db == null)
+            throw new DbNonInitialiseeException();
+
+        ContentValues cv = new ContentValues();
+        cv.put("utilisateur", user);
+        cv.put("defi", defi);
+
+        return db.insert("suggestions", null, cv) != -1;
     }
 
     public enum Type_Defi {
@@ -25,6 +50,6 @@ public class GestionnaireSuggestion {
         CAVALIER,
         FOU,
         REINE,
-        ROI
+        ROI;
     }
 }
